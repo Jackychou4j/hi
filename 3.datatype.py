787cf114326a3353e40eb535dfd56376978a079d
Jacky Chou <jacky4jmain@gmail.com>
2020-11-27 20:09:26 +0800

    Add new file

---
+++ 3.datatype.py
@@ -0,0 +1,14 @@
+#資料型別
+#1.整數 integer / int
+#2.浮點數(小數) float
+#3.布林值 boolean / bool (只有True / False兩種)
+#4.字串 string / str (文字) ex:'Allen'
+price = 10
+pen = 0.38
+rain = True 
+name = 'Allen'
+print(price)
+print(pen)
+print(rain)
+print(name)
+print(price, pen, rain, name)
